251891d574dedfce554476430b04309fc783e1fa
Alex@CodeTunnel.com <Alex@CodeTunnel.com@2b694bfe-3d4a-4b38-f2d4-fd81ccf54c14>
2011-12-06 05:37:20 +0000

    Removed custom code for the HuffAndStapes podcast feed and redirected all requests to http://www.CodeTunnel.com/RSS/HuffAndStapes where the code now resides.

--- trunk/U413.MvcUI/Controllers/RSSController.cs
+++ trunk/U413.MvcUI/Controllers/RSSController.cs
@@ -10,19 +10,9 @@ namespace U413.MvcUI.Controllers
 {
     public class RSSController : Controller
     {
-        public ContentResult HuffAndStapes()
+        public RedirectResult HuffAndStapes()
         {
-            var request = (HttpWebRequest)WebRequest.Create("http://www.heidiandfrank.com/podcast");
-            request.UserAgent = "User-Agent: Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/14.0.835.186 Safari/535.1";
-            XDocument xmlDoc;
-            using (var response = request.GetResponse())
-                xmlDoc = XDocument.Load(response.GetResponseStream());
-            xmlDoc.Elements().First().Elements().First().Elements().Single(x => x.Name.LocalName.Equals("title")).Value = "Huff & Stapes";
-            xmlDoc.Elements().First().Elements().First().Elements().First(x => x.Name.LocalName.Equals("image")).Descendants("title").First().Value = "Huff & Stapes";
-            xmlDoc.Elements().First().Elements().First().Elements().First(x => x.Name.LocalName.Equals("image")).Descendants("url").First().Value = "http://static.huffandstapes.com/podcast/huffstapespodcast.png";
-            xmlDoc.Elements().First().Elements().First().Elements().Last(x => x.Name.LocalName.Equals("image")).Attribute("href").Value = "http://static.huffandstapes.com/podcast/huffstapespodcast.png";
-            xmlDoc.Descendants("item").Where(x => x.Descendants("title").All(y => !y.Value.StartsWith("Huff & Stapes"))).Remove();
-            return this.Content(xmlDoc.ToString(), "text/xml");
+            return RedirectPermanent("http://www.CodeTunnel.com/RSS/HuffAndStapes");
         }
     }
 }
